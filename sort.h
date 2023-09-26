4e6555b3680949d6b899cf49ff58d22c7b5180f8
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-27 01:31:58 +0200

    2-selection_sort.c

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void swap_int(int *x, int *y);
 /* sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
